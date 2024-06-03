e8c56086c746198fb25e7d348b4e30937ffdde47
Tim Potze <timpotze@gmail.com>
2024-06-03 22:43:35 +0200

    added files

---
+++ Generator/GeneratorTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+
+namespace Generator
+{
+    [Generator]
+    public class GeneratorTest : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            // ERROR
+            //
+            // CSC : warning CS8784: Generator 'GeneratorTest' failed to initialize.
+            // It will not contribute to the output and compilation errors may occur as a result.
+            // Exception was of type 'TypeLoadException' with message 'Could not load type 'Generator.Bar' from assembly
+            // 'Generator, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null'.'.
+
+            _ = Bar.Create();
+        }
+    }
+
+    public struct Bar
+    {
+        public ImmutableArray<Bar> Prop { get; }
+
+        public static ImmutableArray<Bar> Create()
+        {
+            return default;
+        }
+    }
+}
---
+++ SourceGenBug/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+
+Foo.Create();
+Console.WriteLine("Hello, World!");
+
+
+public struct Foo
+{
+    // This is fine
+    public ImmutableArray<Foo> Prop { get; }
+
+    public static ImmutableArray<Foo> Create()
+    {
+        return default;
+    }
+}
